af42dd23f7af15e0d72bf1d65eb997c539a41f92
majkelmichel <mich.zaw2002@gmail.com>
2021-09-01 15:01:39 +0200

    Added basic hello world application with tests

---
+++ src/me/majkelmichel/helloworld/HelloWorld.java
@@ -0,0 +1,12 @@
+package me.majkelmichel.helloworld;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println(getHelloWorld());
+    }
+
+    public static String getHelloWorld() {
+        return "Hello World";
+    }
+}
---
+++ src/me/majkelmichel/helloworld/HelloWorldTest.java
@@ -0,0 +1,44 @@
+package me.majkelmichel.helloworld;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloWorldTest {
+    final static ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    final static ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+
+    @BeforeAll
+    static void setUpStreams() {
+
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @org.junit.jupiter.api.Test
+    void main() {
+        final String expected = "Hello World\r\n";
+
+        HelloWorld.main(new String[] {});
+        assertEquals(expected, outContent.toString());
+    }
+
+    @org.junit.jupiter.api.Test
+    void getHelloWorld() {
+        final String expected = "Hello World";
+
+        assertEquals(expected, HelloWorld.getHelloWorld());
+    }
+
+    @AfterAll
+    static void restoreStreams() {
+        final PrintStream originalOut = System.out;
+        final PrintStream originalErr = System.err;
+        System.setOut(originalOut);
+        System.setErr(originalErr);
+    }
+}
